e8471a80c0eed71099051d37b546449ea2f629e7
Gabriel Logan Dias Porto <gabriellogand2@gmail.com>
2023-07-04 11:51:42 -0300

    ta

--- DB4K/script.js
+++ DB4K/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', './start.py', true);
+    xhr.open('GET', '../start.py', true);
     xhr.send();
   });
   
